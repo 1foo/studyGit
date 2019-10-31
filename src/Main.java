0a694b369aa22f772e3e5194de8cfe6c987943c4
Ryan <ryan_97@yeah.net>
2019-10-31 14:03:07 +0800

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        test();
+    }
+
+    public static void test(){
+        System.out.println("1111");
+    }
+}
